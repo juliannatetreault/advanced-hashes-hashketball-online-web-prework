3bb1eb58788378ac667a3f11f7c7ae470aa8393d
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-24 09:25:59 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,7 +200,7 @@ def player_stats(player_name)
         data.each do |player, stats|
           #binding.pry
           if player == player_name
-            return stats.values.to_i
+            return stats.values.to_s
           end
         end
       end
