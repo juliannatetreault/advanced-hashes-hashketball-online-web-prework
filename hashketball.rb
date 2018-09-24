4836dca8b1440b8494b13946949f8034471de1fb
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-24 09:03:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,6 +195,7 @@ def player_stats(player_name)
   game_hash.each do |location, team_hash|
     team_hash.each do |players, data|
       if players == :players
+        data.each do ||
 end
 
 #returns the number of rebounds of the player 
