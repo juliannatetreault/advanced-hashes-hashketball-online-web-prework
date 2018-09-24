6606fc89bd568d9024cef20f975034deb501f2c7
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-24 08:42:11 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,7 @@ end
 
 #returns the player jersey numbers
 def player_numbers(team_name)
-  game_hash.collect do |location, team_data|
+  game_hash.each do |location, team_data|
     #binding.pry
     if team_data[:team_name] == team_name
       return team_data[:number].to_i
